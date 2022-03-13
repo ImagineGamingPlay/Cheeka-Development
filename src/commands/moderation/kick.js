5edc87a6cc177f2d09c56c800dd3e195c15ea993
WithoutIdeas <withoutideias27>
2022-03-13 15:30:40 +0000

    change kick to ban inside string
    not a huge difference, but just to be sure

--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -12,7 +12,7 @@ module.exports= {
         try {
             if(!message.member.permissions.has('KICK_MEMBERS') || !message.guild.me.permissions.has("KICK_MEMBERS")) return;
             let member = message.mentions.members.first();
-            if (!member) await message.reply("You need to mention someone to ban!");
+            if (!member) await message.reply("You need to mention someone to kick!");
             const reason = args.slice(1).join(' ')
             if(!reason) await message.reply("No reason was given");
             await member.send(`You were kicked of ${message.guild.name} for **${reason}**`)
