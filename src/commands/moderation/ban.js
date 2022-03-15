081262c602a94042e6450a01421e08aae1b69638
MaxMady <81635616+MaxMady@users.noreply.github.com>
2022-03-15 07:18:44 +0530

    Commit

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,3 +1,7 @@
+const { DiscordAPIError } = require("discord.js");
+const Discord = require('discord.js')
+
+
 module.exports = {
   name: "ban",
   description: "Bans a member",
@@ -18,14 +22,35 @@ module.exports = {
       let member = message.mentions.members.first();
       if (!member) await message.reply("You need to mention someone to ban!");
       const reason = args.slice(1).join(" ");
-      if (!reason) await message.reply("No reason was given");
-      await member.send(
-        `You were banned of ${message.guild.name} for **${reason}**`
-      );
-      await member.ban();
+      //Notifcation Embed
+      const em = new Discord.MessageEmbed()
+      .setTitle(`Server Ban`)
+      .setFooter(`Banned by ${message.author.tag}`)
+      .setColor(`RED`)
+      //Fetching reason
+      if (!reason) {
+        em.setDescription(`You have been banned from ${message.guild.name} \nIf you think this is a mistake, please appeal in [here](https://discord.com/api/oauth2/authorize?client_id=900535112955998271&redirect_uri=https%3A%2F%2Fimagine.cf%2Fcallback&response_type=code&scope=email%20identify)`)
+      } else {
+        em.setDescription(`You have been banned from ${message.guild.name} for **${reason}** \nIf you think this is a mistake, please appeal in [here](https://discord.com/api/oauth2/authorize?client_id=900535112955998271&redirect_uri=https%3A%2F%2Fimagine.cf%2Fcallback&response_type=code&scope=email%20identify)`)
+      }
+      //Appeal Button
+      const r = new Discord.MessageActionRow()
+      .addComponents(
+        new Discord.MessageButton()
+        .setStyle(`LINK`)
+        .setLabel(`Appeal`)
+        .setURL(`https://discord.com/api/oauth2/authorize?client_id=900535112955998271&redirect_uri=https%3A%2F%2Fimagine.cf%2Fcallback&response_type=code&scope=email%20identify`)
+        .setEmoji(`🔗`)
+      )
+      await member.send({embeds:[em], components:[r]})
+      if(!reason) {
+        await message.reply(`${member.user.tag} was banned`)
+      } else {
       await message.reply(`${member.user.tag} was banned for **${reason}**!`);
+      }
     } catch (e) {
-      message.reply(`${e}`);
+      message.reply(`An error occured: \`\`\`${e}\`\`\``)
+      console.log(e)
     }
   },
 };
