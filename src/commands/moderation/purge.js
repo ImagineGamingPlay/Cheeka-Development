3421f867c6e3169b870dc6d40ac73c20bb3e1194
GoodBoyNeon <neonfavor160@gmail.com>
2022-03-17 12:48:21 +0545

    purge command

---
+++ src/commands/moderation/purge.js
@@ -0,0 +1,34 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "purge",
+  description: "clear a certain amount of messages",
+  aliases: ["clear", "clr"],
+  permission: ["MANAGE_MESSAGES"],
+  run: async ({ client, message, args }) => {
+    const amount = args[0];
+
+    if (amount > 100)
+      return message.reply(
+        "You cannnot delete more than 100 messages at once!"
+      );
+    if (isNaN(amount))
+      return message.reply("Invalid amount given, amount must be a number!");
+
+    try {
+        await message.channel.bulkDelete(amount, true);
+        await message.channel.bulkDelete(1)
+        message.channel.send({ embeds: [
+            new MessageEmbed()
+            .setTitle(`🧹 Successfully deleted ${amount} messages!`)
+            .setColor("BLURPLE")
+        ] }).then(msg => {
+            setTimeout(() => msg.delete(), 5000);
+        })
+    } catch (err) {
+        return;
+      console.log(err);
+      await message.reply(`\`\`\`${err}\`\`\``);
+    }
+  },
+};
