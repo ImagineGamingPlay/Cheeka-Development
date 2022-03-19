f49c3461d6df98fc4b5e8369bf144519b7343c02
javascriptduck <83098851+javascriptduck@users.noreply.github.com>
2022-03-19 08:41:25 -0400

    created servercount command (untested)

---
+++ src/commands/moderation/servercount.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "servercount",
+    description: "view the number of servers the bot is in, and their names.",
+    aliases: ["sc"],
+    permission: ["SEND_MESSAGES"],
+    run: async ({ client, message, args }) => {
+        var guilds = []
+        client.guilds.cache.forEach(guild => {
+            guilds.push(guild.name)
+        })
+        const a = new MessageEmbed()
+        .setAuthor('Servers i\'m in', client.user.displayAvatarURL())
+        .setDescription(guilds.join('\n'))
+        .setFooter(`Currently in ${client.guilds.cache.size} servers.`)
+        message.reply({embeds: [a]})
+    },
+};
