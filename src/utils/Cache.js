40e154823bfc20e3c0d8ee70e5f3a6cffbd1953e
daysling <pangenisushant20@gmail.com>
2022-03-24 16:57:56 +0545

    Created a cache for the rules as well as a RuleObject for export

--- src/utils/Cache.js
+++ src/utils/Cache.js
@@ -7,3 +7,24 @@ module.exports.blackListCache = new Map();
  * @type Map<string, any>
  */
 module.exports.cBlackListCache = new Map();
+
+/**
+ * @type {Map<number, Rule>}
+ */
+module.exports.rulesCache = new Map();
+
+/**
+ * @type {exports.Rule}
+ */
+module.exports.Rule = class Rule {
+    /**
+     * @param {number} index
+     * @param {string} title
+     * @param {string} description
+     */
+    constructor(index, title, description) {
+        this.index = index;
+        this.title = title;
+        this.description = description;
+    }
+};
