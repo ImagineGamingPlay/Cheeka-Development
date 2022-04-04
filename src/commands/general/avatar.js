b93808bef90e97e12020b55090f9da166dd124ad
GoodBoyNeon <neonfavor160@gmail.com>
2022-04-04 18:44:55 +0545

    fixes

--- src/commands/general/avatar.js
+++ src/commands/general/avatar.js
@@ -7,21 +7,29 @@ module.exports = {
   aliases: ["av"],
   usage: "avatar [user]",
   run: async ({ client, message, args }) => {
-    const user =
-      message.mentions.users.first() ||
-      message.guild.members.cache.get(args[0]) ||
-      message.author;
+    try {
+      const user =
+        message.mentions.users.first() ||
+        message.guild.members.cache.get(args[0]) ||
+        message.author;
 
-    const username = user.username || user.user.username;
-    const avatarEmbed = new MessageEmbed()
-      .setColor("BLURPLE")
-      .setTitle(`${username}'s Avatar`)
-      .setImage(user.displayAvatarURL({ dynamic: true, size: 2048 }))
-      .setFooter({
-        text: `Requested by ${message.author.tag}`,
-        iconURL: message.author.displayAvatarURL({ dynamic: true, size: 2048 }),
-      });
+      const username = user.username || user.user.username;
+      const avatarEmbed = new MessageEmbed()
+        .setColor("BLURPLE")
+        .setTitle(`${username}'s Avatar`)
+        .setImage(user.displayAvatarURL({ dynamic: true, size: 2048 }))
+        .setFooter({
+          text: `Requested by ${message.author.tag}`,
+          iconURL: message.author.displayAvatarURL({
+            dynamic: true,
+            size: 2048,
+          }),
+        });
 
-    message.reply({ embeds: [avatarEmbed] });
+      message.reply({ embeds: [avatarEmbed] });
+    } catch (err) {
+      console.error(err);
+      message.reply(`\`\`\`${err}\`\`\``);
+    }
   },
 };
