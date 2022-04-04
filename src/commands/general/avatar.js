34aee9129f06412a862c0b7329c6cc15bee641f3
GoodBoyNeon <neonfavor160@gmail.com>
2022-04-04 18:35:55 +0545

    added avatar command

---
+++ src/commands/general/avatar.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "avatar",
+  description: "Get the avatar of a user",
+  category: "general",
+  aliases: ["av"],
+  usage: "avatar [user]",
+  run: async ({ client, message, args }) => {
+    const user =
+      message.mentions.users.first() ||
+      message.guild.members.cache.get(args[0]) ||
+      message.author;
+
+    const username = user.username || user.user.username;
+    const avatarEmbed = new MessageEmbed()
+      .setColor("BLURPLE")
+      .setTitle(`${username}'s Avatar`)
+      .setImage(user.displayAvatarURL({ dynamic: true, size: 2048 }))
+      .setFooter({
+        text: `Requested by ${message.author.tag}`,
+        iconURL: message.author.displayAvatarURL({ dynamic: true, size: 2048 }),
+      });
+
+    message.reply({ embeds: [avatarEmbed] });
+  },
+};
