2863c616e26ab7189bd4d898f47bfbe4562f67d1
NightSling <pangenisushant20@gmail.com>
2022-04-04 15:18:29 +0000

    Make cooldown and add category

--- src/commands/fun/meme.js
+++ src/commands/fun/meme.js
@@ -7,6 +7,8 @@ module.exports = {
   name: "meme",
   description:
     "get a meme out from r/memes and sends it in the required channel",
+  category: "Fun",
+  cooldown: 5,
   run: async (client, message, args) => {
     https.get(url, (result) => {
       var body = "";
