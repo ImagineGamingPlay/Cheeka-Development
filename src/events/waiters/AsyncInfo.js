fbc5c6a4ebf7a89c672f3afec963454683a8042f
HmmNoPls <90952192+HmmNoPls@users.noreply.github.com>
2022-04-06 20:43:38 +0700

    Update AsyncInfo.js

--- src/events/waiters/AsyncInfo.js
+++ src/events/waiters/AsyncInfo.js
@@ -1,1 +1,45 @@
+const { guildCache } = require("../../utils/Cache");
+const { MessageEmbed } = require("discord.js");
+const {google} = require('googleapis')
+const service = google.youtube({ version: 'v3', auth: "Google API Key" });
 
+module.exports = {
+  // Time will be in milliseconds, run this every 1 minute
+  time: 60000,
+  run: async function (client) {
+    Array.from(guildCache.values()).forEach(async (guild) => {
+      if(!guild.infoChannel) return;
+      let channel = await client.channels
+        .fetch(guild?.infoChannel)
+        .catch((error) => {
+          console.log(error);
+        });
+      let message = await channel.messages
+        .fetch(guild?.infoMessage)
+        .catch((error) => console.log(error));
+      let results = await service.channels.list({
+        part: ['snippet', 'statistics', 'contentDetails'],
+        id: 'UCzBQ65qoUGqNPcbiNQN2pJA'
+      })
+      let result = results.data.items[0]
+      let videos = await service.playlistItems.list({
+        part: ['snippet', 'status'],
+        playlistId: result.contentDetails.relatedPlaylists.uploads
+      })
+      await message?.send({
+        embeds: [
+          new MessageEmbed()
+            .setTitle("Channel Statistics")
+            .addFields(
+              {name: "Subscriber Count", value: result.statistics.subscriberCount},
+              {name: "View Count", value: result.statistics.viewCount, inline: true},
+              {name: "Video Count", value: result.statistics.videoCount, inline: true},
+              {name: "Last Uploaded Video", value: videos.data.items[0]}
+            )
+            .setColor("#2200ff")
+            .setThumbnail(result.snippet.thumbnails.medium.url),
+        ],
+      })
+    })
+  },
+};
