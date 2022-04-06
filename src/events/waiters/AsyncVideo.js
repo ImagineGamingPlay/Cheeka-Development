cb13d0e3e017fdb5539ebaad2cd14a7629657bc4
HmmNoPls <90952192+HmmNoPls@users.noreply.github.com>
2022-04-06 21:29:35 +0700

    Update AsyncVideo.js

--- src/events/waiters/AsyncVideo.js
+++ src/events/waiters/AsyncVideo.js
@@ -1,1 +1,33 @@
+const { guildCache } = require("../../utils/Cache");
+const { MessageEmbed } = require("discord.js");
+const {google} = require('googleapis')
+const service = google.youtube({ version: 'v3', auth: "Google API Key" });
 
+module.exports = {
+  // Time will be in milliseconds, run this every 1 minute
+  time: 60000,
+  run: async function (client) {
+    Array.from(guildCache.values()).forEach(async (guild) => {
+      if(!guild.videoChannel) return;
+      let channel = await client.channels
+        .fetch(guild?.videoChannel)
+        .catch((error) => {
+          console.log(error);
+        });
+      let results = await service.channels.list({
+        part: ['snippet', 'statistics', 'contentDetails'],
+        id: 'UCzBQ65qoUGqNPcbiNQN2pJA'
+      })
+      let result = results.data.items[0]
+      let videos = await service.playlistItems.list({
+        part: ['snippet', 'status'],
+        playlistId: result.contentDetails.relatedPlaylists.uploads
+      })
+      let content = "Imagine just uploaded a video!\n" + videos.data.items[0]
+      if(guild.videoRole) content = guild.videoRole.toString() + content
+      await channel?.send({
+        content: content
+      })
+    })
+  },
+};
