bb44402ba3e2887574825285b24e32993f11e300
GoodBoyNeon <neonfavor160@gmail.com>
2022-04-09 13:42:05 +0545

    index file cleanup

---
+++ src/handlers/error.js
@@ -0,0 +1,48 @@
+process.on("unhandledRejection", (reason, p) => {
+  console.log(reason, p);
+  client.channels.cache.get("958000637944164462").send({
+    embeds: [
+      new MessageEmbed()
+        .setTitle("Unhandled Rejection")
+        .setDescription(`${reason}`)
+        .setColor("RED")
+        .addField("Stack", "```js\n" + reason.stack + "```"),
+    ],
+  });
+});
+process.on("uncaughtException", (err, origin) => {
+  console.log(err, origin);
+  client.channels.cache.get("958000637944164462").send({
+    embeds: [
+      new MessageEmbed()
+        .setTitle("Uncaught Exception")
+        .setDescription(`${err}`)
+        .setColor("RED")
+        .addField("Stack", "```js\n" + err.stack + "```"),
+    ],
+  });
+});
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+  console.log(err, origin);
+  client.channels.cache.get("958000637944164462").send({
+    embeds: [
+      new MessageEmbed()
+        .setTitle("Uncaught Exception")
+        .setDescription(`${err}`)
+        .setColor("RED")
+        .addField("Stack", "```js\n" + err.stack + "```"),
+    ],
+  });
+});
+process.on("multipleResolves", (type, promise, reason) => {
+  console.log(type, promise, reason);
+  client.channels.cache.get("958000637944164462").send({
+    embeds: [
+      new MessageEmbed()
+        .setTitle("Multiple Resolves")
+        .setDescription(`${type}`)
+        .setColor("RED")
+        .addField("Stack", "```js\n" + reason.stack + "```"),
+    ],
+  });
+});
--- src/index.js
+++ src/index.js
@@ -17,59 +17,8 @@ const config = (global.config = require("../config.json"));
 
 module.exports = { client };
 
-require("./handlers/events");
-require("./handlers/commands");
-require("./handlers/mongoose");
+["commands", "events", "mongoose", "error"].forEach((handler) => {
+  require(`./handlers/${handler}`);
+});
 
 client.login(process.env.token).catch((e) => console.log(e));
-
-//<------------- PROCESS ERROR HANDLING ------------->
-process.on("unhandledRejection", (reason, p) => {
-  console.log(reason, p);
-  client.channels.cache.get("958000637944164462").send({
-    embeds: [
-      new MessageEmbed()
-        .setTitle("Unhandled Rejection")
-        .setDescription(`${reason}`)
-        .setColor("RED")
-        .addField("Stack", "```js\n" + reason.stack + "```"),
-    ],
-  });
-});
-process.on("uncaughtException", (err, origin) => {
-  console.log(err, origin);
-  client.channels.cache.get("958000637944164462").send({
-    embeds: [
-      new MessageEmbed()
-        .setTitle("Uncaught Exception")
-        .setDescription(`${err}`)
-        .setColor("RED")
-        .addField("Stack", "```js\n" + err.stack + "```"),
-    ],
-  });
-});
-process.on("uncaughtExceptionMonitor", (err, origin) => {
-  console.log(err, origin);
-  client.channels.cache.get("958000637944164462").send({
-    embeds: [
-      new MessageEmbed()
-        .setTitle("Uncaught Exception")
-        .setDescription(`${err}`)
-        .setColor("RED")
-        .addField("Stack", "```js\n" + err.stack + "```"),
-    ],
-  });
-});
-process.on("multipleResolves", (type, promise, reason) => {
-  console.log(type, promise, reason);
-  client.channels.cache.get("958000637944164462").send({
-    embeds: [
-      new MessageEmbed()
-        .setTitle("Multiple Resolves")
-        .setDescription(`${type}`)
-        .setColor("RED")
-        .addField("Stack", "```js\n" + reason.stack + "```"),
-    ],
-  });
-});
-//<----------------PROCESS ERROR HANDLING END----------------->
