9a5b456c58ad31de426f692c527e1f65aa14c6f1
N44 <nandhu90040@gmail.com>
2022-04-13 19:04:48 +0530

    Commented this file to prevent es6 , cjs conflicts

--- src/structure/CommandStructure.js
+++ src/structure/CommandStructure.js
@@ -1,16 +1,16 @@
 // Export a default client, message and args structure
 
-import { Message } from "discord.js";
-import { client } from "..";
+// import * as discordJs from "discord.js";
+// import { client } from "..";
 
-export const CommandStructure = {
-  client: client,
-  /**
-   * @type {Message}
-   */
-  message: Message,
-  /**
-   * @type {string[]} args
-   */
-  args: [],
-};
+// export const CommandStructure = {
+//   client: client,
+//   /**
+//    * @type {discordJs.Message}
+//    */
+//   message: discordJs.Message,
+//   /**
+//    * @type {string[]} args
+//    */
+//   args: [],
+// };
