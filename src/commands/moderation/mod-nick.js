083adcfbd0752b144b5a5f90cd4f7b9b04230b36
GoodBoyNeon <neonfavor160@gmail.com>
2022-04-14 22:12:55 +0545

    moderate nickanme command

---
+++ src/commands/moderation/mod-nick.js
@@ -0,0 +1,34 @@
+module.exports = {
+	name: "mod-nick",
+	description: "Change a user's nickname",
+	aliases: ["modnick", "moderate-nick", "moderate-nick"],
+	category: "moderation",
+	permissions: ["MANAGE_NICKNAMES"],
+	/**
+	 * @param {CommandStructure} message
+	 * @param {string} args
+	 * @returns {(Promise<Message|boolean>)}
+	 */
+	run: async ({ client, message, args }) => {
+		const target =
+			message.mentions.members.first() ||
+			message.guild.members.cache.get(args[0]);
+
+		if (!target) {
+			return message.reply("Please mention a user or provide a user ID");
+		}
+
+		const characters =
+			"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		let nickname = "";
+
+		for (let i = 0; i < 5; i++) {
+			const ran = Math.floor(Math.random() * characters.length);
+			nickname += characters.substring(ran, ran + 1);
+		}
+
+		target.setNickname(`Moderated nickname ${nickname}`, [
+			`Moderated nickname triggered by ${message.author.tag}`,
+		]);
+	},
+};
