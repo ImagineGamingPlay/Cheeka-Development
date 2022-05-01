6e52b45be92011a0dc2219955850acd37ca4e392
GoodBoyNeon <neonfavor160@gmail.com>
2022-05-01 20:52:59 +0545

    fixed tip system
    replaced the channel id from test server channel to main server channel

--- src/functions/tip.js
+++ src/functions/tip.js
@@ -1,11 +1,11 @@
+const tipsSchema = require("../schema/tips");
 
 client.on("typingStart", typing => {
-	if (typing.channel.id !== "954618018560868392") return;
+	if (typing.channel.id !== "743528053019508848") return;
 	const tips = [
-		"You can dm the bot to create a modmail?! You can directly talk to the staff team or the bot development team!",
+		"You can dm the bot to create a modmail?! You can directly talk to the staff team or the bot development team through this feature!",
 		"We got fun commands, too? Try `-help` and select the **fun** category! We got commands like 8ball, meme, hack etc!",
 	]; // TODO: Add schema for tips
-
 
 	const frequency = 40;
 	const randomTip = Math.floor(Math.random() * tips.length);
