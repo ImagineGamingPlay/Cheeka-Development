3f1e9fd04c0e4194fc936dca31610b2d101a76a1
HmmNoPls <90952192+HmmNoPls@users.noreply.github.com>
2022-07-19 13:27:17 +0700

    Update Esnipe.js

--- src/events/Esnipe.js
+++ src/events/Esnipe.js
@@ -7,7 +7,7 @@ module.exports = {
   async execute(oldMessage, newMessage, client) {
     //collect data and add to snipes
     if (newMessage.author?.bot) return;
-    const division = 1950; //as an embed description can only have upto 4096 characters, we are dividing it to fit both old and new messages
+    //as an embed description can only have upto 4096 characters, we are dividing it to fit both old and new messages
     // Set the message to eSnipe
     let eSnipes = client.eSnipe.get(newMessage.channel.id)
     eSnipes.push({
