8c18f367a4476df051dea92da8784bb8fe46248f
daysling <pangenisushant20@gmail.com>
2022-07-27 18:47:50 +0545

    Missing ';'

--- src/events/modalCollector.js
+++ src/events/modalCollector.js
@@ -10,8 +10,8 @@ module.exports = {
 	 * @returns
 	 */
 	async execute(interaction, client) {
-		if(!interaction.isModalSubmit()) return
-    		let collector = client.modalCollector.get(interaction.message.id)
-    		if(collector && collector instanceof Discord.Collector) collector.emit(`collect`, interaction)
+		if(!interaction.isModalSubmit()) return;
+    		let collector = client.modalCollector.get(interaction.message.id);
+    		if(collector && collector instanceof Discord.Collector) collector.emit(`collect`, interaction);
 	},
 };
