e31fd7f519d5a12f15820f729af5057f83ef368c
unknown <ngynag@gmail.com>
2022-12-16 11:20:32 +0700

    Maybe advanced kick command

--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const CommandStructure =
   require('../../structure/CommandStructure').CommandStructure;
 module.exports = {
@@ -13,19 +14,36 @@ module.exports = {
    * @param {CommandStructure}
    * @returns {Promise<*>}
    */
-  run: async ({client, message, args}) => {
+  run: async ({ client, message, args }) => {
     try {
       let member =
         message.mentions.members.first() ||
         message.guild.members.cache.get(args[0]);
-      if (!member) return message.reply('You need to mention someone to kick!');
-      const reason = args.slice(1).join(' ');
-      if (!reason) return message.reply('No reason was given');
-      await member.send(
-        `You were kicked of ${message.guild.name} for **${reason}**`,
-      );
-      await member.kick();
-      await message.reply(`${member.user.tag} was kicked!`);
+      if (!member) return message.reply({
+        embeds: [
+          new Discord.MessageEmbed()
+            .setDescription("Please give the user id or mention a user to kick!")
+        ]
+      }).then((msg) => {
+        setTimeout(() => {
+          msg.delete();
+        }, 5000)
+      })
+      let reason = "No reason provided.";
+      if (args[1]) {
+        reason = args[1].join(" ");
+      }
+      await member.send({
+        embeds: [
+          new Discord.MessageEmbed()
+            .setDescription(
+              `You were kicked out from ${message.guild.name} for the following reason: **${reason}**`
+            )
+        ]
+      })
+      await member.kick(reason);
+      let msg = await message.reply(`${member.user.tag} was kicked because of **${reason}**`);
+      setTimeout(() => { msg.delete() }, 5000)
     } catch (e) {
       await message.reply(`${e}`);
     }
