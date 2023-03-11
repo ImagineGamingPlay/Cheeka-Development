b7f6891a36cbf25d0375e3d213686d3aa1c0a3fd
GoodBoyNeon <goodboyneon03@gmail.com>
2023-03-11 14:10:05 +0545

    promoTimeout Dm message

---
+++ src/data/idData.ts
@@ -0,0 +1,5 @@
+export const channels = {};
+
+export const categories = {
+  promotionCategoryId: '1076494954811097109',
+};
--- src/features/promoTimeout.ts
+++ src/features/promoTimeout.ts
@@ -1,32 +1,52 @@
+import { EmbedOptions, Message } from 'eris';
 import { client } from '..';
 import { promoBlacklist } from '../types';
+import { messages } from '../data/messages';
+import { categories } from '../data';
+
+const timeoutChannel = async (
+  message: Message,
+  channelId: string,
+  limit: number
+) => {
+  let blacklist: promoBlacklist[] = [];
+
+  if (message.channel.id == channelId) {
+    const alreadyBlacklist = blacklist.some(
+      (obj: promoBlacklist) => obj.id === message.author.id
+    );
+    if (alreadyBlacklist) {
+      await message.delete();
+      const guild = client.guilds.get(message?.guildID || '');
+      const embed: EmbedOptions = {
+        title: messages.promoBlacklistFail.title,
+        description: messages.promoBlacklistFail.description,
+        color: client.config.colors.red,
+        author: {
+          name: guild?.name || 'IGP',
+          icon_url: guild?.dynamicIconURL() || '',
+        },
+      };
+      const dmChannel = await message.author.getDMChannel();
+      dmChannel.createMessage({ embeds: [embed] });
+
+      return;
+    }
+    blacklist.forEach(obj => {
+      obj.index--;
+    });
+
+    blacklist = blacklist.filter(obj => obj.index > 0);
+    blacklist.push({ id: message.author.id, index: limit });
+  }
+};
 
 export const promoTimeout = () => {
-  const promoChannelId = '1074881123794046991';
-  let blacklist: promoBlacklist[] = [];
+  // const promoChannelId = '1074881123794046991';
 
   client.on('messageCreate', async message => {
     if (message.author.bot) {
       return;
-    }
-
-    if (message.channel.id == promoChannelId) {
-      const alreadyBlacklist = blacklist.some(
-        (obj: promoBlacklist) => obj.id === message.author.id
-      );
-      if (alreadyBlacklist) {
-        // client.createMessage(message.channel.id, {
-        //   content: 'You are currently blacklisted!',
-        // });
-        await message.delete();
-        return;
-      }
-      blacklist.forEach(obj => {
-        obj.index--;
-      });
-
-      blacklist = blacklist.filter(obj => obj.index > 0);
-      blacklist.push({ id: message.author.id, index: 1 });
     }
   });
 };
