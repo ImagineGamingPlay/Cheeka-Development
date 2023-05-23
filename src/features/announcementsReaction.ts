a1a2eaccaa887bae0a8c27c8304d6bb0f67d21a4
Aljo <aljo.berginc@gmail.com>
2023-05-23 15:05:16 +0200

    removed console logs

--- src/features/announcementsReaction.ts
+++ src/features/announcementsReaction.ts
@@ -16,13 +16,11 @@ export const announcementsReaction = (message: Message) => {
         max_tokens: 50,
         temperature: 0.9
     }).then(async resp => {
-        console.log(resp.data.choices[0].text);
         let r = resp.data.choices[0].text as string;
         r = r.replace("'", "\"");
         try {
             JSON.parse(r.match(/\[.*?\]/)![0]).forEach((e: EmojiIdentifierResolvable) => message.react(e).catch(() => announcementsReaction(message)));
         } catch(err) {
-            console.log(err);
             announcementsReaction(message);
         }
     }).catch(() => announcementsReaction(message));
