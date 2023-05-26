ce181fe384158e6d736605471b08d9eee4b3418c
Aljo <aljo.berginc@gmail.com>
2023-05-26 15:22:56 +0200

    removed debug stuff

--- src/features/announcementsReaction.ts
+++ src/features/announcementsReaction.ts
@@ -1,7 +1,6 @@
 import { EmojiIdentifierResolvable, Message } from 'discord.js';
 import { config } from "../config";
 import { OpenAIApi, Configuration } from "openai";
-import { error } from 'console';
 export const announcementsReaction = (message: Message, timesCalled?: number) => {
     if(!timesCalled) timesCalled = 0;
     if(timesCalled > (config.aiReactionTimesCalled || 5)) return;
@@ -18,16 +17,12 @@ export const announcementsReaction = (message: Message, timesCalled?: number) =>
         max_tokens: 50,
         temperature: 0.9
     }).then(async resp => {
-        console.log(resp.data);
         let r = resp.data.choices[0].text as string;
         r = r.replace("'", "\"");
         try {
             JSON.parse(r.match(/\[.*?\]/)![0]).forEach((e: EmojiIdentifierResolvable) => message.react(e).catch(() => announcementsReaction(message, timesCalled)));
-        } catch(err) {
-            console.log(err);
+        } catch {
             announcementsReaction(message, timesCalled);
         }
-    }).catch(err => {
-        announcementsReaction(message, timesCalled);
-    });
+    }).catch(() => announcementsReaction(message, timesCalled));
 };
