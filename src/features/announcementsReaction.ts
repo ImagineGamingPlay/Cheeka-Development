a5dbda75908db3a5f2e22e0a880ad904fa23f994
Aljo <aljo.berginc@gmail.com>
2023-05-26 15:23:36 +0200

    i forgot the prompt

--- src/features/announcementsReaction.ts
+++ src/features/announcementsReaction.ts
@@ -9,7 +9,8 @@ export const announcementsReaction = (message: Message, timesCalled?: number) =>
     openai.createCompletion({
         model: "text-davinci-003",
         prompt: `
-        
+        React to the messages with emojis only. Use a JSON array of strings to include the emojis. The array can have 3 or 4 emojis. If you can only think of 1 or 2 relevant emojis, that's fine too. Don't add irrelevant emojis just to meet the desired length. However, don't exceed more than 3 or 4 emojis. Keep the most relevant emojis at the beginning of the array. Only provide the array as your response, no other text.
+
         Text: ${message.content}
 
         Assistant:
