5bc106d24d7f0e6a6ca4306c88d39ba6899203aa
heisenberg <ngynag@gmail.com>
2023-07-03 12:27:21 +0700

    added hushky and help cmd option back

--- src/commands/general/help.ts
+++ src/commands/general/help.ts
@@ -6,7 +6,8 @@ import {
 } from 'discord.js';
 import { Command } from '../../lib/classes/Command';
 import { getFiles } from '../../utils';
-
+import { CommandType } from '../../types';
+import * as path from 'path';
 export default new Command({
     name: 'help',
     description: 'View the connection status',
@@ -20,8 +21,9 @@ export default new Command({
     ],
     run: async ({ interaction, client, options }) => {
         const category = options?.getString('category');
-        const categories = getFiles(`${__dirname}/../../commands`, false);
-        console.log(categories)
+        const categories = getFiles(`${__dirname}/../../commands`, false).map(
+            obj => obj.split('/').pop()
+        );
         if (!category) {
             const helpEmbed = new EmbedBuilder()
                 .setTitle(`Select a category`)
@@ -52,6 +54,33 @@ export default new Command({
             } catch (e) {
                 console.log(e);
             }
-        } 
+        } else {
+            const commands: CommandType[] = [];
+            const commandsFiles = getFiles(
+                `./src/commands`, true
+            ).filter(
+                dir => dir.includes(`/commands/${category}`)
+            );
+            for (const file of commandsFiles) {
+                const filePath = path.resolve(__dirname, '..', file).replace('/src/commands', '')
+                const commandClass: CommandType = (await import(filePath)).default;
+                const { ...command } = commandClass;
+
+                commands.push(command);
+            }
+            let commandsData = '';
+            for (let i = 0; i < commands.length; i++) {
+                const command = commands[i];
+                commandsData += `**${command.name}** - ${command.description}\n`;
+            }
+            const messageEmbed = new EmbedBuilder()
+                .setTitle(`Help | ${category}`)
+                .setDescription(commandsData)
+                .setColor(client.config.colors.blurple);
+
+            await interaction.followUp({
+                embeds: [messageEmbed],
+            });
+        }
     },
 });
