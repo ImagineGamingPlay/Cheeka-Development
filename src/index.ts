0959f3825c9e4fa8fe06455767edffcce8e365cc
GoodBoyNeon <goodboyneon03@gmail.com>
2023-07-20 16:23:31 +0545

    add error handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { Cheeka } from './lib';
-import { setWizardConfig } from 'console-wizard';
+import { logger, setWizardConfig } from 'console-wizard';
 
 setWizardConfig({
     includeStatus: true,
@@ -9,7 +9,15 @@ setWizardConfig({
 
 export const client: Cheeka = new Cheeka();
 
-// client.on('debug', console.log).on('warn', console.log);
-// client.once('ready', () => console.log('Ready - from index.ts'));
-
 client.deploy();
+
+process.on('uncaughtException', error => {
+    logger.error(error.name);
+    console.log(
+        `${error.message}\nCause: ${error.cause}\nStack: ${error.stack}`
+    );
+});
+process.on('unhandledRejection', (error, promise) => {
+    logger.error(`Unhandled Promise: ${promise}`);
+    console.log(error);
+});
