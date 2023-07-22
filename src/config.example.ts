b3a62571ec5987f09087492deb7cfb11040ba28d
GoodBoyNeon <goodboyneon03@gmail.com>
2023-07-22 20:25:52 +0545

    fix: add config.ts to gitignore

---
+++ src/config.example.ts
@@ -0,0 +1,25 @@
+/* Imports */
+import { ConfigType } from './types';
+
+export const config: ConfigType = {
+    environment: '',
+    openaiApiKey: '',
+
+    aiReactionChannels: [],
+
+    token: '',
+    clientId: '',
+    guildId: '',
+
+    devGuildId: '',
+    mainGuildId: '',
+
+    developerRoleId: '',
+
+    colors: {
+        blurple: 0xffffff,
+        red: 0xffffff,
+        green: 0xffffff,
+        white: 0xffffff,
+    },
+};
